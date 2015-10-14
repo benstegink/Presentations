63dcad5aa20871b1d0089caac1a3de8f56221912
Ben Stegink <bstegink@intelligink.com>
2015-10-14 09:29:48 -0400

    update to 01-BasicInformation.ps1

---
+++ PowerShell/Live360-SharePoint-OnPrem/01-BasicInformation.ps1
@@ -0,0 +1,12 @@
+$bp = Set-PSBreakpoint -Variable wait -Mode Write -Script $psISE.CurrentFile.FullPath
+
+#Get all Web Applications
+
+$wait = "Here"
+#Get all Site Collections
+
+$wait = "Here"
+#Get all Sites
+
+$wait = "here"
+
